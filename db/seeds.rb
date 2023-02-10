1605276aa8677fe9a41ae13bc66c23036c6285e6
Sidharth Baveja <sidharthbav@gmail.com>
2023-02-10 06:04:07 -0600

    updating the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,11 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'About Time', :rating => 'R',
+    :release_date => '1-Nov-2013'},
+  {:title => 'King Richard', :rating => 'PG-13',
+    :release_date => '19-Nov-2021'}
 ]
 
 more_movies.each do |movie|
